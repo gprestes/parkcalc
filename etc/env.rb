1bef567528c3a2355cc06461b515dc7bd83687e8
Guilherme Prestes <guilherme.prestes@gmail.com>
2012-08-05 13:26:06 -0300

    changed from spec to rspec and using relative path for lib

--- etc/env.rb
+++ etc/env.rb
@@ -1,15 +1,15 @@
 require 'rubygems'
 gem 'selenium-client'
 require 'selenium/client'
-require 'spec/spectations'
-require 'lib/parkcalc'
+require 'rspec/expectations'
+require './lib/parkcalc'
 
 #before all
 selenium_driver = Selenium::Client::Driver.new \
   :host => 'localhost',
   :port => 4444,
   :browser => '*firefox',
-  :url => 'http://www.shino.de/parkcalc',
+  :url => 'http://www.shino.de',
   :timeout_in_second => 60
 selenium_driver.start_new_browser_session
 $park_calc = ParkCalcPage.new(selenium_driver)
