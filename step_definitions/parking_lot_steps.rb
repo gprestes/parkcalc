122dfe5a0f51153865b4794d11b007e9d8b16bd9
Guilherme Prestes <guilherme.prestes@gmail.com>
2012-08-05 13:28:15 -0300

    steps now (try to) do something

--- step_definitions/parking_lot_steps.rb
+++ step_definitions/parking_lot_steps.rb
@@ -1,8 +1,11 @@
 When /^I park my car in the Valet Parking Lot for (.*)$/ do |duration|
+  $parkcalc.select('Valet Parking')
+  $parkcalc.enter_parking_duration(duration)
   pending
 end
 
 Then /^I will have to pay (.*)$/ do |price|
-    pending
+  $parkcalc.parking_costs.should == price
+  pending
 end
 
