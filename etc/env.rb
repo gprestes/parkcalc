700b793547ccb6ef2b5055741c0c390ecddbe81c
Guilherme Prestes <guilherme.prestes@gmail.com>
2012-08-15 00:44:04 -0300

    fixed page name

--- etc/env.rb
+++ etc/env.rb
@@ -9,10 +9,10 @@ selenium_driver = Selenium::Client::Driver.new \
   :host => 'localhost',
   :port => 4444,
   :browser => '*firefox',
-  :url => 'http://www.shino.de',
+  :url => 'http://www.shino.de/parkcalc',
   :timeout_in_second => 60
 selenium_driver.start_new_browser_session
-$park_calc = ParkCalcPage.new(selenium_driver)
+$parkcalc = ParkCalcPage.new(selenium_driver)
 
 #after all
 at_exit do
